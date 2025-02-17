efa410e139bffe46510b96dd8aa6b7c9e3747d51
Daniel Bittencourt <danieltartarob@gmail.com>
2025-02-16 21:56:34 -0300

    Exercícios

--- linguagem-de-programacao/n1/src/ex01/Quadrado.java
+++ linguagem-de-programacao/n1/src/ex01/Quadrado.java
@@ -1,5 +1,11 @@
 package ex01;
 
-public class Quadrado {
+import javax.swing.JOptionPane;
 
+public class Quadrado {
+	public static void main(String[] args) {
+		int lado = Integer.parseInt(JOptionPane.showInputDialog("Digite o tamanho de um dos lados do quadrado"));
+		
+		JOptionPane.showMessageDialog(null, "Área: " + (lado * lado));
+	}
 }
---
+++ linguagem-de-programacao/n1/src/ex02/Reajuste.java
@@ -0,0 +1,15 @@
+package ex02;
+
+import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
+
+public class Reajuste {
+
+	public static void main(String[] args) {
+		double salario = Double.parseDouble(JOptionPane.showInputDialog("Digite o salário a ser reajustado"));
+		DecimalFormat df = new DecimalFormat("#.##");
+		
+		JOptionPane.showMessageDialog(null, "Salário com aumento de 15%: R$" + (salario + (salario * 0.15)));
+	}
+
+}
