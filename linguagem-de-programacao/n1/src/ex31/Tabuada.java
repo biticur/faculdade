bc6b871aa6badf5243ae849376a8615abf93d062
Daniel Bittencourt <danieltartarob@gmail.com>
2025-02-18 23:27:36 -0300

    Create Tabuada.java

---
+++ linguagem-de-programacao/n1/src/ex31/Tabuada.java
@@ -0,0 +1,22 @@
+package ex31;
+
+import javax.swing.JOptionPane;
+
+public class Tabuada {
+	public static void calcularTabuada(int num, int multiplicador) {
+		if(multiplicador > 10) {
+			return;
+		} else {
+			System.out.println(num + " x " + multiplicador + " = " + (num * multiplicador));
+			calcularTabuada(num, multiplicador + 1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		int num = Integer.parseInt(JOptionPane.showInputDialog("Digite um número"));
+		int multiplicador = 1;
+		System.out.println("Tabuada\n==============");
+		calcularTabuada(num, multiplicador);
+	}
+
+}
